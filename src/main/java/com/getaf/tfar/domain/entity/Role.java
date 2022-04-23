18bc4b55a0d594eea8b7d0ecb28e8c72cd84b9b9
hatem.bellaaj <hatem.bellaaj@magnolia.fr>
2022-04-23 09:23:51 +0100

    fix Role class for security process

--- src/main/java/com/getaf/tfar/domain/entity/Role.java
+++ src/main/java/com/getaf/tfar/domain/entity/Role.java
@@ -1,23 +1,25 @@
 package com.getaf.tfar.domain.entity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+
 import javax.persistence.Table;
+
+
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+
 import lombok.Setter;
 import lombok.ToString;
 
 
 
-@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
@@ -26,21 +28,27 @@ import lombok.ToString;
 @Entity
 @Table(name = "role")
 public class Role {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="id_role")
-	private Long idRole;
-	
-	@Column(name="titre")
-	private String titre;
+	private Integer id;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name="name",length = 20)
+	private ERole name;
 	
 	@Column(name="description")
 	private String description;
 	
 	@Column(name="deleted")
 	private Boolean deleted;
+
+
+	public Role() {
+
+	}
 	
-	@ManyToOne(optional=false)
-    @JoinColumn(name="code_user",referencedColumnName="code")
-	private User user;
+	
+	
 }
