46b79d8800aa4dd3e77c27d68c50d83605a514fe
hatem.bellaaj <hatem.bellaaj@magnolia.fr>
2022-04-23 09:45:03 +0100

    Adjust UserRepository class for security process

--- src/main/java/com/getaf/tfar/repository/UserRepository.java
+++ src/main/java/com/getaf/tfar/repository/UserRepository.java
@@ -1,4 +1,6 @@
 package com.getaf.tfar.repository;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -6,4 +8,11 @@ import com.getaf.tfar.domain.entity.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
+	Optional<User> findByLogin(String login);
+
+	Boolean existsByLogin(String login);
+
+	Boolean existsByEmail(String email);
+	
+	
 }
